6e15fdb49a4facb104ab3cc7b8933e0ad5ffd665
Elvinas V <elvinas.val@gmail.com>
2021-12-06 17:51:58 +0200

    feat: Add bootstrap

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import App from './App';
 import { store } from './app/store';
 
