df461317f7e8cfab5611c21114efcad51095fc55
Elvinas V <elvinas.val@gmail.com>
2022-01-04 16:40:52 +0200

    feat: Fetch initial data in AppWrapper

--- src/index.js
+++ src/index.js
@@ -8,14 +8,8 @@ import './custom.scss';
 
 import { App } from './App';
 import { store } from './app/store';
-import { fetchUsers } from './features/users/usersSlice';
-import { fetchQuestions } from './features/question/questionsSlice';
 
 const persistor = persistStore(store);
-
-// Fetch the list only once when the application starts.
-store.dispatch(fetchUsers());
-store.dispatch(fetchQuestions());
 
 ReactDOM.render(
   <React.StrictMode>
